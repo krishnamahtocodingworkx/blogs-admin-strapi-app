f71ebe87cb7a68b4ff02ed0bd81b81e843d8536b
Krishna Mahto <krishna.mahto@codingworkx.com>
2024-11-15 11:51:25 +0530

    cloudinary added

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,21 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
